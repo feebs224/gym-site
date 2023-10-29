9b949dade3f7dfe57a3016702d3e6e7cf18f384f
Fibi Youssef <fibi057057@gmail.com>
2023-10-29 14:03:07 -0500

    finishing left side of hero

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -1,6 +1,10 @@
 import React from "react";
 import "./Hero.css";
 import Header from "../Header/Header";
+import hero_image from "../../assets/hero_image.png";
+import hero_image_back from "../../assets/hero_image_back.png";
+import Heart from "../../assets/heart.png";
+import Calories from "../../assets/calories.png";
 
 const Hero = () => {
   return (
@@ -21,10 +25,51 @@ const Hero = () => {
           <div>
             <span>Ideal Body</span>
           </div>
+          <div class="subheading">
+            <span>Let us help you shape your ideal body!</span>
+          </div>
+        </div>
+
+        <div className="figures">
+          <div>
+            <span>+140</span>
+            <span>expert coaches</span>
+          </div>
+          <div>
+            <span>+978</span>
+            <span>members joined</span>
+          </div>
+          <div>
+            <span>+50</span>
+            <span>fitness programs</span>
+          </div>
+        </div>
+
+        <div className="hero-buttons">
+          <button className="btn">Get Started</button>
+          <button className="btn">Learn More</button>
         </div>
       </div>
+      <div className="right-h">
+        <button className="btn">Join Now!</button>
 
-      <div className="right-h">right side</div>
+        <div className="heart-rate">
+          <img src={Heart} alt="heart emoji" />
+          <span>Heart Rate</span>
+          <span>116 bpm</span>
+        </div>
+
+        <img
+          src={hero_image}
+          alt="athletic man tying shoelaces"
+          className="hero-image"
+        />
+        <img
+          src={hero_image_back}
+          alt="athletic man tying shoelaces"
+          className="hero-image-back"
+        />
+      </div>
     </div>
   );
 };
